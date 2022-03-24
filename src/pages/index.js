b74d7c3d8e55525a8cd2ca416f336c98981d79c0
NamrataRaikwar2002 <92174796+NamrataRaikwar2002@users.noreply.github.com>
2022-03-24 13:08:25 +0530

    feat:import all component in one file

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export {Home} from './Home/Home';
+export {Login} from './Authentication/Login';
+export {Signup} from './Authentication/Signup';
+export {ProductList} from './Product/ProductList';
+export {Wishlist} from './Wishlist/Wishlist';
+export {Cart} from './Cart/Cart';
+
