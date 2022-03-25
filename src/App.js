931b481040adb7ff4c67a61aaa608b631533bd83
NamrataRaikwar2002 <92174796+NamrataRaikwar2002@users.noreply.github.com>
2022-03-25 11:39:05 +0530

    feat:added route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css'
-import { Home } from './pages/Home/Home'
+import {Routes, Route} from 'react-router-dom'
+import { Cart, Home, Login, ProductList, Signup, Wishlist } from './pages'
 
 function App() {
   return (
     <div className="App">
-      <Home />
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/product-list' element={<ProductList />} />
+      <Route path='/login-page' element= {<Login />}/>
+      <Route path='/wishlist-page' element={<Wishlist />}/>
+      <Route path='/cart-page' element={<Cart />} />
+      <Route path='/signup-page' element={<Signup />}/>
+    </Routes>
+    
     </div>
   )
 }
