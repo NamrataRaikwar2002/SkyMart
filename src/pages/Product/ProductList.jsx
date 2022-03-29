8bc63d155c76f221d4bf9d35a415e9662bba8ed5
NamrataRaikwar2002 <92174796+NamrataRaikwar2002@users.noreply.github.com>
2022-03-29 14:55:52 +0530

    refactor: destructuring in maping

--- src/pages/Product/ProductList.jsx
+++ src/pages/Product/ProductList.jsx
@@ -23,28 +23,28 @@ export const ProductList = () => {
           <aside className="product_list">
             <h3 className="productHeading">Showing All Products</h3>
             <section className="product_list_section">
-              {res.map((productItem) => {
+              {res.map(({productImg,price,title,prePrice,discount,rating}) => {
                 return (
                   <div className="product_list_item">
                     <img
                       className="productListImg"
-                      src={productItem.productImg}
+                      src={productImg}
                       alt="images"
                     />
                     <div className="card_detail">
                       <button className="wishlist_icon">
                         <i className="far fa-heart"></i>
                       </button>
-                      <p>{productItem.title}</p>
+                      <p>{title}</p>
                       <div className="mrpdiv">
-                        <h3>₹{productItem.price}</h3>
-                        <s>₹{productItem.prePrice}</s>
+                        <h3>₹{price}</h3>
+                        <s>₹{prePrice}</s>
                       </div>
                       <div className="discountRating">
-                        <p className="green">{productItem.discount}</p>
+                        <p className="green">{discount}</p>
 
                         <p className="ratingStarPara">
-                          {productItem.rating}
+                          {rating}
                           <i className="fas fa-star star_icon"></i>
                         </p>
                       </div>
