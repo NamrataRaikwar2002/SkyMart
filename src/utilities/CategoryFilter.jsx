0d8141566b0127d9b319055a986c7da9dcbddb4e
NamrataRaikwar2002 <92174796+NamrataRaikwar2002@users.noreply.github.com>
2022-04-05 23:08:05 +0530

    feat: added category component

---
+++ src/utilities/CategoryFilter.jsx
@@ -0,0 +1,38 @@
+const CategoryFilter = (
+  products,
+  { Deodorant, Perfume, Fragrance, BodySpray },
+) => {
+  // console.log('line 2',products);
+  let allCategoryList = []
+  if (
+    Deodorant === false &&
+    Perfume === false &&
+    Fragrance === false &&
+    BodySpray === false
+  ) {
+    allCategoryList = products
+  }
+  if (Deodorant) {
+    allCategoryList = allCategoryList.concat(
+      products.filter((item) => item.category === 'Deodorant'),
+    )
+  }
+  if (Perfume) {
+    allCategoryList = allCategoryList.concat(
+      products.filter((item) => item.category === 'Perfume'),
+    )
+  }
+  if (Fragrance) {
+    allCategoryList = allCategoryList.concat(
+      products.filter((item) => item.category === 'Fragrance'),
+    )
+  }
+  if (BodySpray) {
+    allCategoryList = allCategoryList.concat(
+      products.filter((item) => item.category === 'BodySpray'),
+    )
+  }
+  return allCategoryList
+}
+
+export { CategoryFilter }
