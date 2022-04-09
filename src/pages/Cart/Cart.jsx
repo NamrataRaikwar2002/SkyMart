b4d5df364ea1aa46d2e1d48229b6773f2d5b0fd8
NamrataRaikwar2002 <92174796+NamrataRaikwar2002@users.noreply.github.com>
2022-04-09 19:53:35 +0530

    feat: cart management and img for empty cart

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,44 +1,46 @@
 import React from 'react'
-import { product11Img } from '../../assets'
 import { Navbar } from '../../components'
 import './Cart.css'
 import { CartCard } from './component/CartCard'
-
+import { useProduct } from '../../hooks/context/productContext'
+import { emptyCart } from '../../assets';
+import {Link} from 'react-router-dom';
 
 export const Cart = () => {
+const {productState} = useProduct();
+const {cartList} = productState; 
+const totalPrice = cartList.reduce((total, item) => total =  (total + Number(item.prePrice))*item.quantity,0)
+const totalDiscount = totalPrice*45/100
+
   return (
     <main className="cart_page">
-        
         <Navbar 
         LoginOrSignup='Login'
         address='/login-page'
 
         />
         <section className="cart_management content">
+            <h2 className='productHeading'>MY CART({cartList.length})</h2>
+                {cartList.length >=1 ? 
+                <>
             <div className="section_background">
-                <h2>MY CART(1)</h2>
+                <div className='cartListDiv'>
+                   <CartCard />
+                </div>
+                {/* <div className='priceImgDiv'> */}
+
                 <div className="cart_price">
-                   <CartCard 
-                       productImg={product11Img}
-                       title='THE MAN COMPANY Non-Gas Body fragrance'
-                       price='405'
-                       prePrice='689'
-                       discount='45% off'   
-                   />
 
                     <div className="price_details_div">
                         <h3>PRICE DETAILS</h3>
                         <hr/>
                         <div className="price_details">
-                            <div className="each_price_detail price">Price(1 item)
-                                <p>&#8377;2,999</p>
+                            <div className="each_price_detail price">Price({cartList.length} item)
+                                <p>₹{totalPrice}</p>
                             </div>
-
                             <div className="each_price_detail discount"> Discount
-                                <p>-&#8377;1,749</p>
-
+                                <p>-{totalDiscount}</p>
                             </div>
-
                             <div className=" each_price_detail delivery_charges">Delivery Charges
                                 <p className="green">Free</p>
                             </div>
@@ -46,16 +48,30 @@ export const Cart = () => {
                         <hr/>
                         <div className="total_amount">
                             <h4>TOTAL AMOUNT</h4>
-                            <h4>₹1,250</h4>
+                            <h4>{totalPrice-totalDiscount}</h4>
                         </div>
                         <hr/>
-                        <p className="green">You will save ₹1,749 on this order</p>
+                        <p className="green">You will save ₹{totalDiscount} on this order</p>
                         <div className="place_order">
                             <button className="e_com_btn primary_selected_btn">PLACE ORDER</button>
                         </div>
                     </div>
                 </div>
             </div>
+                </>
+                : <>
+                <div className='emptycartDiv'>
+                <img src={emptyCart} alt="empty cart" className='emptyCartImg' />
+                <p>
+                <Link to='/product-list'>
+                <button className='emptycartBtn card_btn'>Shop Now</button>
+                </Link>
+                </p>
+                </div>
+                </>
+            }
+
+
         </section>
 
     </main>
--- src/pages/Cart/component/CartCard.jsx
+++ src/pages/Cart/component/CartCard.jsx
@@ -1,36 +1,97 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useProduct } from '../../../hooks/context/productContext'
 
-export const CartCard = (props) => {
+export const CartCard = () => {
+  const { productState, productDispatch } = useProduct()
+  const { cartList } = productState
 
-  // const quantityFun = (quantity) => {
-  //   if(quantity === 'quantityInc'){
-      
-  //   }
-  // }
   return (
-    <div className=" horizontal_card cartHorizontalCard">
-      <section className="horizontal_img_section">
-        <img className="img1 horizontal_img" src={props.productImg} />
-        <div className="card_title horizontal_text">
-          <h3 className="heading">{props.title}</h3>
-          <div className="mrp">
-            <h3>₹{props.price}</h3>
-            <s>₹{props.prePrice}</s>
-          </div>
-          <p className="green">{props.discount}</p>
-          <small>
-            Quantity:
-            <button className="quantity_btn decrease_btn">-</button>
-            <input type="text" className="quantity_input" value="1"  />
-            <button className="quantity_btn">+</button>
-          </small>
-          <button className="horizontal_cart_btn">Remove From Cart</button>
-          <Link to='/wishlist-page'>
-          <button className="e_com_btn wishlist_btn">Move to Wishlist</button>
-          </Link>
-        </div>
-      </section>
-    </div>
+    <>
+      {cartList.map(
+        ({
+          productImg,
+          price,
+          title,
+          prePrice,
+          rating,
+          discount,
+          _id,
+          quantity,
+        }) => {
+          return (
+            <div className="horizontal_card cartHorizontalCard" key={_id}>
+              <section className="horizontal_img_section">
+                <img className="img1 horizontal_img" src={productImg} />
+                <div className="card_title horizontal_text">
+                  <h3 className="heading">{title}</h3>
+                  <div className="mrp">
+                    <h3>₹{price}</h3>
+                    <s>₹{prePrice}</s>
+                  </div>
+                  <p className="green">{discount}</p>
+                  <small className="quantity_div">
+                    Quantity:
+                    <button
+                      className="quantity_btn decrease_btn"
+                      onClick={() =>
+                        productDispatch({
+                          type: 'DECREASE_QUANTITY',
+                          payload: { _id: _id, quantity: quantity },
+                        })
+                      }
+                    >
+                      -
+                    </button>
+                    <p className="quantity_input">{quantity}</p>
+                    {/* <p type="text" className="quantity_input" value={quantity} /> */}
+                    <button
+                      className="quantity_btn"
+                      onClick={() =>
+                        productDispatch({
+                          type: 'INCREASE_QUANTITY',
+                          payload: { _id: _id, quantity: quantity },
+                        })
+                      }
+                    >
+                      +
+                    </button>
+                  </small>
+                  <button
+                    className="horizontal_cart_btn"
+                    onClick={() =>
+                      productDispatch({
+                        type: 'REMOVE_FROM_CART',
+                        payload: { _id: _id },
+                      })
+                    }
+                  >
+                    Remove From Cart
+                  </button>
+                  <button
+                    className="e_com_btn wishlist_btn"
+                    onClick={() =>
+                      productDispatch({
+                        type: 'MOVE_TO_WISHLIST',
+                        payload: {
+                          productImg: productImg,
+                          price: price,
+                          title: title,
+                          prePrice: prePrice,
+                          discount: discount,
+                          rating: rating,
+                          _id: _id,
+                        },
+                      })
+                    }
+                  >
+                    Move to Wishlist
+                  </button>
+                </div>
+              </section>
+            </div>
+          )
+        },
+      )}
+    </>
   )
 }
