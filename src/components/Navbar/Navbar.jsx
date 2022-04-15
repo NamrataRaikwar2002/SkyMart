a7ce3b146dc00d990ea5364b44d32094b226085e
NamrataRaikwar2002 <92174796+NamrataRaikwar2002@users.noreply.github.com>
2022-04-15 23:15:44 +0530

    feat: fevicon added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import './Navbar.css'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { useProduct } from '../../hooks/context/productContext'
+import { feviconImg } from '../../assets'
 
 const Navbar = ({ LoginOrSignup, address }) => {
-  const {productState} = useProduct();
-  const {cartList, wishList} = productState;
+  const { productState } = useProduct()
+  const { cartList, wishList } = productState
   return (
     <div className="nav_div">
       <nav className="navigation">
-    <Link to='/'>
-        <h2 className='textForPrimaryColor'>SkyMart</h2></Link>
+        <Link to="/" className="appNameWithIcon">
+          <img src={feviconImg} alt="fevicon" className="feviconImg" />
+          <h2 className="textForPrimaryColor">SkyMart</h2>
+        </Link>
         <div className="search_icon">
           <input
             type="text"
@@ -21,19 +24,25 @@ const Navbar = ({ LoginOrSignup, address }) => {
           <i className="fa fa-search"></i>
         </div>
         <aside className="nav_rightside">
-
-          <Link to='/product-list' className="navbar_link textForPrimaryColor productsText">Products</Link>
+          <Link
+            to="/product-list"
+            className="navbar_link textForPrimaryColor productsText"
+          >
+            Products
+          </Link>
           <Link to={address}>
             <button className="nav_btn navbar_link">{LoginOrSignup}</button>
           </Link>
-          <Link to='/wishlist-page'>
+          <Link to="/wishlist-page">
             <i
               className="far fa-heart nav_icon wishlist_nav_icon"
               id="comIcon"
             ></i>
-            <p className="wishlist_icon_home_badge icon_badge">{wishList.length}</p>
+            <p className="wishlist_icon_home_badge icon_badge">
+              {wishList.length}
+            </p>
           </Link>
-          <Link to='/cart-page'>
+          <Link to="/cart-page">
             <i className="fas fa-shopping-cart nav_icon cart_nav_icon"></i>
             <p className="cart_icon_home_badge icon_badge">{cartList.length}</p>
           </Link>
